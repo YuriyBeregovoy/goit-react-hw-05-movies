300230298428560d3d030edfd730c5d4b6325a17
YuriyBeregovoy <rik0681@gmail.com>
2023-09-03 13:36:19 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,11 @@
 export const App = () => {
+
+  const KEY = '18e447cd4ab696665fa7fbc918675bb1';
+  const readAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOGU0NDdjZDRhYjY5NjY2NWZhN2ZiYzkxODY3NWJiMSIsInN1YiI6IjY0ZjQ1YzE0Y2FhNTA4MDBhYjczNjE1YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6vt_mYcbEyyitdAlRBqukGv8W-Hk2KsX0x7BkYzF7R0"
+
+
+
+
   return (
     <div>
     
