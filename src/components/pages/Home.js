1ff38b2a2f7484b092b88203cd0cada0d6bb95fd
YuriyBeregovoy <rik0681@gmail.com>
2023-09-05 18:42:47 +0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 const Home = () => {
+ const [trendingMoviesArrea, setTrendingMoviesArrea] = useState([]);
+
+
   useEffect(() => {
+   const fetchMovies = async () => {
 
-    // HTTP запит
-  
-}, [])
+      try {
+   
+        const response = await axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=18e447cd4ab696665fa7fbc918675bb1");
+
+        console.log(response.data.results);
+        const trendingMoviesArray = response.data.results;
+        setTrendingMoviesArrea(trendingMoviesArray);
+
+      } catch (error) {
+        console.error("Помилка при отриманні даних:", error);
+        throw error;
+      }
+    }
+    fetchMovies();
+    }, []);
 
 
 
-  return <div>Home</div>
+  return (<div>{trendingMoviesArrea.map(movie => {
+    return <div key={movie.id}>{movie.title}</div> 
+  })}</div>)
 }
 
 export default Home;
