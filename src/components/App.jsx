aaa115e64a99cc97434002d56d506c05e7010057
YuriyBeregovoy <rik0681@gmail.com>
2023-09-06 19:23:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { NavLink, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
 
 export const App = () => {
 
@@ -14,7 +15,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/movies" element={<Movies/>} />
-        <Route path="/movies/:movieId" element={<div>MovieDetails</div>}>
+        <Route path="/movies/:movieId" element={<MovieDetails/>}>
           <Route path="/movies/:movieId/cast" element={<div>Cast</div>} />
           <Route path="/movies/:movieId/reviews" element={<div>Reviews</div>} />
         </Route>
