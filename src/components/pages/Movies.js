6e80b90ece702a08e1953ff91178df4cc5d4737f
YuriyBeregovoy <rik0681@gmail.com>
2023-09-06 19:50:13 +0300

    Update Movies.js

--- src/components/pages/Movies.js
+++ src/components/pages/Movies.js
@@ -1,6 +1,49 @@
-const Movies = () => {
 
-  return <div>Movies</div>
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+const Movies = () => {
+//  const [trendingMoviesArrea, setTrendingMoviesArrea] = useState([]);
+
+
+//   useEffect(() => {
+//    const fetchMovies = async () => {
+
+//       try {
+   
+//         const response = await axios.get("https://api.themoviedb.org/3/trending/movie/day?api_key=18e447cd4ab696665fa7fbc918675bb1");
+
+//         // console.log(response.data.results);
+//         const trendingMoviesArray = response.data.results;
+//         setTrendingMoviesArrea(trendingMoviesArray);
+
+//       } catch (error) {
+//         console.error("Помилка при отриманні даних:", error);
+//         throw error;
+//       }
+//     }
+//     fetchMovies();
+//     }, []);
+
+
+
+  return (
+    <main>
+      <div>
+      
+    </div>
+    </main>
+  )
 }
 
 export default Movies;
+
+
+
+
+
+
+
+
+
