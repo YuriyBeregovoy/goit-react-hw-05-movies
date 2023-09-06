bd60139c8f5cf5ebf70e4f488f605fa584ff9db5
YuriyBeregovoy <rik0681@gmail.com>
2023-09-06 20:14:10 +0300

    Update Movies.js

--- src/components/pages/Movies.js
+++ src/components/pages/Movies.js
@@ -1,38 +1,55 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
+// import Notiflix from 'notiflix';
+import { Link } from "react-router-dom";
 
 
 const Movies = () => {
-//  const [trendingMoviesArrea, setTrendingMoviesArrea] = useState([]);
+ const [trendingMoviesArrea, setTrendingMoviesArrea] = useState([]);
 
 
-//   useEffect(() => {
-//    const fetchMovies = async () => {
+  useEffect(() => {
+   const fetchMovies = async (searchQuery) => {
 
-//       try {
+      try {
    
-//         const response = await axios.get("https://api.themoviedb.org/3/trending/movie/day?api_key=18e447cd4ab696665fa7fbc918675bb1");
+        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1&api_key=18e447cd4ab696665fa7fbc918675bb1`);
 
-//         // console.log(response.data.results);
-//         const trendingMoviesArray = response.data.results;
-//         setTrendingMoviesArrea(trendingMoviesArray);
+        console.log(response.data.results);
+        const trendingMoviesArray = response.data.results;
+        setTrendingMoviesArrea(trendingMoviesArray);
 
-//       } catch (error) {
-//         console.error("Помилка при отриманні даних:", error);
-//         throw error;
-//       }
-//     }
-//     fetchMovies();
-//     }, []);
+      } catch (error) {
+        console.error("Помилка при отриманні даних:", error);
+        throw error;
+      }
+    }
+    fetchMovies();
+    }, []);
 
 
 
   return (
     <main>
-      <div>
-      
-    </div>
+      <form onSubmit={evt => {
+        evt.preventDefault();
+        const searchQuery = evt.target.elements.query.value.trim();
+        if (searchQuery) { changeQuery(searchQuery); } else {
+          // Notiflix.Notify.failure('Please enter a valid search query.');
+        }
+        evt.target.reset();
+      }}>
+          <input type="text" name="query" placeholder="Search movies..."/>
+          <button type="submit">Search</button>
+      </form>
+       <ul>
+          <li>
+        {trendingMoviesArrea.map(movie => {
+        return <Link key={movie.id} to={`${movie.id}`}>{movie.title}</Link> 
+        })}
+          </li>
+      </ul>
     </main>
   )
 }
