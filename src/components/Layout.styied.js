54030d0d10a91bcd44050c9a37cf6499ab1d6566
YuriyBeregovoy <rik0681@gmail.com>
2023-09-08 22:12:00 +0300

    Create Layout.styied.js

---
+++ src/components/Layout.styied.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const LayoutStyles = styled.div`
+        position: relative;
+        display: flex;
+        flex-direction: column;
+
+        max-width: 1158px;
+        width: 100%;
+        padding-left: 15px;
+        padding-right: 15px;
+        margin-right: auto;
+        margin-left: auto;
+
+  padding-bottom: 24px;
+   align-items: center;
+
+
+`;
