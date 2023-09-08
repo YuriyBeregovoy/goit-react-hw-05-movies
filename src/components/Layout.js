5b6cff58d1e61ddb12a20656af2edbe08a62cf6a
YuriyBeregovoy <rik0681@gmail.com>
2023-09-08 12:55:27 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,19 @@
+import { NavLink, Outlet } from "react-router-dom"
 
 
 
 export const Layout = () => {
 
   return (
-  
+    <div>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </div>
     
 )
 
