ed34b7daf8574729dcc02a671cdbc86efb997fe3
YuriyBeregovoy <rik0681@gmail.com>
2023-09-08 21:21:15 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { NavLink, Outlet } from "react-router-dom"
 
 
@@ -13,7 +14,9 @@ export const Layout = () => {
         </nav>
       </header>
       <main>
-        <Outlet />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
       </main>
     </div>
     
