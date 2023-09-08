6408c4e1519256e4f193ca37be99127ed3bd9da6
YuriyBeregovoy <rik0681@gmail.com>
2023-09-08 21:48:14 +0300

    Create GlobalStyle.js

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,73 @@
+import { createGlobalStyle } from "styled-components";
+import "modern-normalize";
+
+
+
+export const GlobalStyle = createGlobalStyle`
+
+html {
+  box-sizing: border-box;
+  width: 100vw;
+  overflow-x: hidden;
+  
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+  color: #212121;
+  background-color: #fff;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+a {
+  color: currentColor;
+  text-decoration: none;
+}
+
+button {
+  cursor: pointer;
+}
+
+
+
+
+address{
+  font-style: normal;
+}
+
+
+
+`;
