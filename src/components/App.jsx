1c37c727325c6792aba7e24cc3c33c004cf30cca
YuriyBeregovoy <rik0681@gmail.com>
2023-09-08 12:55:31 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,26 @@
-import { NavLink, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
 import Cast from "./Cast";
 import Reviews from "./Reviews";
+import { Layout } from "./Layout";
 
 export const App = () => {
 
 
   return (
-    <div>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/movies">Movies</NavLink>
-      </nav>
+          
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/movies" element={<Movies/>} />
-        <Route path="/movies/:movieId" element={<MovieDetails/>}>
-          <Route path="/movies/:movieId/cast" element={<Cast/>} />
-          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="/movies" element={<Movies/>} />
+          <Route path="/movies/:movieId" element={<MovieDetails/>}>
+            <Route path="/movies/:movieId/cast" element={<Cast/>} />
+            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
-    </div>
+    
   );
 };
