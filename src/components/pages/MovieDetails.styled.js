a1658a7fb7365087f5bc99518cd47debacbf3e19
YuriyBeregovoy <rik0681@gmail.com>
2023-09-09 00:30:27 +0300

    Update MovieDetails.styled.js

--- src/components/pages/MovieDetails.styled.js
+++ src/components/pages/MovieDetails.styled.js
@@ -4,3 +4,10 @@ import styled from "styled-components";
 export const DetailsLinkStyles = styled.div`
 margin-top: 20px;
 `;
+
+export const AdditionalInfoStyles = styled.p`
+margin-top: 20px;
+margin-bottom: 20px;
+
+font-weight: 500;
+`;
