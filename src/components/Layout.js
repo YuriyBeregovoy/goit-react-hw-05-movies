afcb78b83b50c8ddf72b72b504c3bfdd74d23406
YuriyBeregovoy <rik0681@gmail.com>
2023-09-08 12:57:11 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,10 +6,12 @@ export const Layout = () => {
 
   return (
     <div>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/movies">Movies</NavLink>
-      </nav>
+      <header>
+          <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/movies">Movies</NavLink>
+        </nav>
+      </header>
       <main>
         <Outlet />
       </main>
