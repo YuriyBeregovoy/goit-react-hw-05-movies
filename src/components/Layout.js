d3d92f321f8534d5e56686f349b1640d9e2b4dfc
YuriyBeregovoy <rik0681@gmail.com>
2023-09-08 22:27:30 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,25 +1,28 @@
 import { Suspense } from "react"
 import { NavLink, Outlet } from "react-router-dom"
-import { LayoutStyles } from "./Layout.styied"
+import { HeaderStyles, LayoutStyles } from "./Layout.styied"
 
 
 
 export const Layout = () => {
 
   return (
-    <LayoutStyles>
-      <header>
-          <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/movies">Movies</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Outlet />
-        </Suspense>
-      </main>
-    </LayoutStyles>
+    
+     <div>
+        <HeaderStyles>
+            <nav>
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/movies">Movies</NavLink>
+          </nav>
+        </HeaderStyles>
+      <LayoutStyles>
+        <main>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </main>
+      </LayoutStyles>
+     </div>
     
 )
 
