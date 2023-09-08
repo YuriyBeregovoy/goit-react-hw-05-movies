6c532a639d3588fb69cdd0d0b272f585dcf7422b
YuriyBeregovoy <rik0681@gmail.com>
2023-09-08 18:12:32 +0300

    Update Movies.js

--- src/components/pages/Movies.js
+++ src/components/pages/Movies.js
@@ -1,28 +1,22 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Notiflix from 'notiflix';
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 
 const Movies = () => {
- const [searchMoviesArray, setSearchMoviesArray] = useState([]);
-  const [searchQuery, setsearchQuery] = useState("");
-  
-  const changeQuery = (newQuery) => {
-    if (newQuery !== "") {
-      setsearchQuery(newQuery);
-      setSearchMoviesArray([]);
-    }
-  };
+  const [searchMoviesArray, setSearchMoviesArray] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query");
+
 
   useEffect(() => {
     const fetchMovies = async () => {
-      if (searchQuery) {
+      if (query) {
 
         try {
    
-          const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1&api_key=18e447cd4ab696665fa7fbc918675bb1`);
+          const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1&api_key=18e447cd4ab696665fa7fbc918675bb1`);
 
           // console.log(response.data.results);
           const searchMoviesArray = response.data.results;
@@ -36,23 +30,23 @@ const Movies = () => {
      
     }
      fetchMovies();
-    }, [searchQuery]);
+    }, [query]);
 
-
+  const handleSubmit = e => {
+    e.preventDefault();
+     const searchQuery = e.target.elements.query.value.trim();
+    setSearchParams({ query: searchQuery });
+     e.target.reset();
+  };
 
   return (
     <main>
-      <form onSubmit={evt => {
-        evt.preventDefault();
-        const searchQuery = evt.target.elements.query.value.trim();
-        if (searchQuery) { changeQuery(searchQuery); } else {
-          Notiflix.Notify.failure('Please enter a valid search query.');
-        }
-        evt.target.reset();
-      }}>
-          <input type="text" name="query" placeholder="Search movies..."/>
-          <button type="submit">Search</button>
+     
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="query" />
+        <button type="submit">Search</button>
       </form>
+     
        <ul>
           
         {searchMoviesArray.map(movie => {
@@ -65,12 +59,5 @@ const Movies = () => {
 }
 
 export default Movies;
-
-
-
-
-
-
-
 
 
