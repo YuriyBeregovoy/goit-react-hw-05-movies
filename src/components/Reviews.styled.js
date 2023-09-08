aa520275013e77d08ba84b05bbe02cd025af21d7
YuriyBeregovoy <rik0681@gmail.com>
2023-09-09 00:40:23 +0300

    Create Reviews.styled.js

---
+++ src/components/Reviews.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const ReviewStyles = styled.li`
+margin-top: 20px;
+margin-bottom: 20px;
+`;
+
+export const AuthorReviewStyles = styled.p`
+
+margin-bottom: 5px;
+font-weight: 500;
+        font-size: 16px;
+        line-height: 1.5;
+        letter-spacing: 0.04em;
+`;
